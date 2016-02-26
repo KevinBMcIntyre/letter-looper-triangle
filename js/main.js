c1cb6f098d4cc57f66c7ec9d2678d649631cfc1d
Kevin McIntyre <kevin@Kevins-MacBook-Pro.local>
2016-02-26 10:06:33 -0500

    fixed indentation

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var num = 1;
 
 do {
   for (i = 0; i < num; i++) {
-  document.write(letter);
-}
-  document.write('<br>')
+    document.write(letter);
+  }
+  document.write('<br>');
   num++;
 } while (num <= 10);
